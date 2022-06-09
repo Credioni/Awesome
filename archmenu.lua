fa40c806b996841e379b860c16075b1a0ee86ce4
Credioni <panu.pulliainen@gmail.com>
2022-06-09 09:02:08 +0300

    main
    main

---
+++ archmenu.lua
@@ -0,0 +1,122 @@
+ local menu98edb85b00d9527ad5acebe451b3fae6 = {
+     {"Archive Manager", "file-roller ", "/usr/share/icons/hicolor/16x16/apps/file-roller.png" },
+     {"Calculator", "gnome-calculator"},
+     {"Character Map", "gucharmap"},
+     {"Desktop Search", "tracker-needle"},
+     {"Disks", "gnome-disks", "/usr/share/icons/hicolor/16x16/apps/gnome-disks.png" },
+     {"Files", "nautilus --new-window ", "/usr/share/icons/hicolor/16x16/apps/org.gnome.Nautilus.png" },
+     {"Font Viewer", "gnome-font-viewer "},
+     {"Gyazo", "gyazo", "/usr/share/pixmaps/gyazo.png" },
+     {"Root Terminal", "gksu -l gnome-terminal", "/usr/share/pixmaps/gksu-root-terminal.png" },
+     {"Screenshot", "gnome-screenshot --interactive"},
+     {"Sublime Text", "subl ", "/usr/share/icons/hicolor/16x16/apps/sublime-text.png" },
+ }
+
+ local menu78059f1898ed518c6ccd6a6392fa82c1 = {
+     {"AUR", "xdg-open https://aur.archlinux.org", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Bugs", "xdg-open https://bugs.archlinux.org", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Developers", "xdg-open http://www.archlinux.org/developers/", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Documentation", "xdg-open https://wiki.archlinux.org/index.php/Official_Arch_Linux_Install_Guide", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Donate", "xdg-open http://www.archlinux.org/donate/", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Forum", "xdg-open https://bbs.archlinux.org", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Homepage", "xdg-open http://www.archlinux.org", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"SVN", "xdg-open http://projects.archlinux.org/svntogit/", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Schwag", "xdg-open http://www.zazzle.com/archlinux/", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Wiki", "xdg-open https://wiki.archlinux.org", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+ }
+
+ local menu251bd8143891238ecedc306508e29017 = {
+     {"Battle for Wesnoth", "wesnoth", "/usr/share/pixmaps/wesnoth-icon.png" },
+     {"Battle for Wesnoth Map Editor", "wesnoth -e", "/usr/share/pixmaps/wesnoth_editor-icon.png" },
+     {"Dwarf Fortress", "dwarffortress", "///usr/share/pixmaps/dwarffortress.png" },
+     {"Feed The Beast", "feedthebeast", "/usr/share/icons/hicolor/16x16/apps/feedthebeast.png" },
+     {"League of Legends", "/usr/share/playonlinux/playonlinux --run \"League of Legends\" ", "///home/credion/.PlayOnLinux//icones/full_size/League of Legends" },
+     {"PlayOnLinux", "playonlinux", "///usr/share/playonlinux/etc/playonlinux.png" },
+     {"Steam", "/usr/bin/steam ", "/usr/share/icons/hicolor/16x16/apps/steam.png" },
+     {"Steam (Native)", "/usr/bin/steam-native ", "/usr/share/icons/hicolor/16x16/apps/steam.png" },
+ }
+
+ local menud334dfcea59127bedfcdbe0a3ee7f494 = {
+     {"Blender", "blender ", "/usr/share/icons/hicolor/16x16/apps/blender.png" },
+     {"Document Viewer", "evince ", "/usr/share/icons/hicolor/16x16/apps/evince.png" },
+     {"GNU Image Manipulation Program", "gimp-2.8 ", "/usr/share/icons/hicolor/16x16/apps/gimp.png" },
+     {"Gyazo", "gyazo", "/usr/share/pixmaps/gyazo.png" },
+     {"Image Viewer", "eog ", "/usr/share/icons/hicolor/16x16/apps/eog.png" },
+ }
+
+ local menuc8205c7636e728d448c2774e6a4a944b = {
+     {"Avahi SSH Server Browser", "/usr/bin/bssh"},
+     {"Avahi VNC Server Browser", "/usr/bin/bvnc"},
+     {"Chromium", "chromium ", "/usr/share/icons/hicolor/16x16/apps/chromium.png" },
+     {"Discord Canary", "/usr/bin/discord-canary", "///usr/share/pixmaps/discord-canary.png" },
+     {"Empathy", "empathy", "/usr/share/icons/hicolor/16x16/apps/empathy.png" },
+     {"Firefox", "firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
+     {"Liri", "liri-browser", "/usr/share/icons/hicolor/16x16/apps/liri-browser.png" },
+     {"Midori", "midori ", "/usr/share/icons/hicolor/16x16/apps/midori.png" },
+     {"Midori Private Browsing", "midori --private ", "/usr/share/icons/hicolor/16x16/apps/midori.png" },
+     {"Mumble", "mumble "},
+     {"Steam", "/usr/bin/steam ", "/usr/share/icons/hicolor/16x16/apps/steam.png" },
+     {"Steam (Native)", "/usr/bin/steam-native ", "/usr/share/icons/hicolor/16x16/apps/steam.png" },
+     {"TeamSpeak 3", "teamspeak3"},
+     {"Transmission", "transmission-gtk ", "/usr/share/icons/hicolor/16x16/apps/transmission.png" },
+     {"Web", "epiphany "},
+ }
+
+ local menudf814135652a5a308fea15bff37ea284 = {
+     {"Dictionary", "gnome-dictionary"},
+     {"Document Viewer", "evince ", "/usr/share/icons/hicolor/16x16/apps/evince.png" },
+ }
+
+ local menue6f43c40ab1c07cd29e4e83e4ef6bf85 = {
+     {"CMake", "cmake-gui ", "/usr/share/icons/hicolor/32x32/apps/CMakeSetup.png" },
+     {"MonoDevelop", "monodevelop ", "/usr/share/icons/hicolor/16x16/apps/monodevelop.png" },
+     {"MonoDevelop (Unity)", "/usr/bin/monodevelop-unity", "/usr/share/icons/hicolor/48x48/apps/unity-monodevelop.png" },
+     {"OpenJDK 1.7.0 Policy Tool", "_BINDIR_/policytool"},
+     {"Qt4 Assistant ", "assistant-qt4", "/usr/share/icons/hicolor/32x32/apps/assistant-qt4.png" },
+     {"Qt4 Designer", "designer-qt4", "/usr/share/icons/hicolor/128x128/apps/designer-qt4.png" },
+     {"Qt4 Linguist ", "linguist-qt4", "/usr/share/icons/hicolor/16x16/apps/linguist-qt4.png" },
+     {"Qt4 QDbusViewer ", "qdbusviewer-qt4", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer-qt4.png" },
+     {"Unity", "/usr/bin/unity-editor", "/usr/share/icons/hicolor/256x256/apps/unity-editor-icon.png" },
+ }
+
+ local menu52dd1c847264a75f400961bfb4d1c849 = {
+     {"Cheese", "cheese", "/usr/share/icons/hicolor/16x16/apps/org.gnome.Cheese.png" },
+     {"FFADO Mixer", "ffado-mixer", "/usr/share/pixmaps/ffado.png" },
+     {"Feed The Beast", "feedthebeast", "/usr/share/icons/hicolor/16x16/apps/feedthebeast.png" },
+     {"PulseAudio Volume Control", "pavucontrol", "/usr/share/icons/hicolor/16x16/apps/multimedia-volume-control.png" },
+     {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
+     {"VLC media player", "/usr/bin/vlc --started-from-file ", "/usr/share/icons/hicolor/16x16/apps/vlc.png" },
+     {"Videos", "totem ", "/usr/share/icons/hicolor/16x16/apps/org.gnome.Totem.png" },
+ }
+
+ local menuee69799670a33f75d45c57d1d1cd0ab3 = {
+     {"Avahi Zeroconf Browser", "/usr/bin/avahi-discover"},
+     {"Color Profile Viewer", "gcm-viewer", "/usr/share/icons/hicolor/16x16/apps/gnome-color-manager.png" },
+     {"Disk Usage Analyzer", "baobab", "/usr/share/icons/hicolor/16x16/apps/baobab.png" },
+     {"GParted", "/usr/bin/gparted_polkit ", "/usr/share/icons/hicolor/16x16/apps/gparted.png" },
+     {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
+     {"Oracle VM VirtualBox", "VirtualBox ", "/usr/share/icons/hicolor/16x16/mimetypes/virtualbox.png" },
+     {"Package Updater", "gpk-update-viewer"},
+     {"Packages", "gpk-application"},
+     {"Startup Disk Creator", "usb-creator-kde"},
+     {"System Log", "gnome-system-log", "/usr/share/icons/hicolor/16x16/apps/logview.png" },
+     {"System Monitor", "gnome-system-monitor"},
+     {"Terminal", "gnome-terminal"},
+     {"UNetbootin", "/usr/bin/unetbootin", "/usr/share/icons/hicolor/16x16/apps/unetbootin.png" },
+     {"UXTerm", "uxterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+     {"XTerm", "xterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+     {"dconf Editor", "dconf-editor", "/usr/share/icons/hicolor/16x16/apps/dconf-editor.png" },
+ }
+
+xdgmenu = {
+    {"Accessories", menu98edb85b00d9527ad5acebe451b3fae6},
+    {"Archlinux", menu78059f1898ed518c6ccd6a6392fa82c1},
+    {"Games", menu251bd8143891238ecedc306508e29017},
+    {"Graphics", menud334dfcea59127bedfcdbe0a3ee7f494},
+    {"Internet", menuc8205c7636e728d448c2774e6a4a944b},
+    {"Office", menudf814135652a5a308fea15bff37ea284},
+    {"Programming", menue6f43c40ab1c07cd29e4e83e4ef6bf85},
+    {"Sound & Video", menu52dd1c847264a75f400961bfb4d1c849},
+    {"System Tools", menuee69799670a33f75d45c57d1d1cd0ab3},
+}
+
